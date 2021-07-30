849d50ff7b82299a1f850f07ed8956764b277770
Ret2Me <37419029+Ret2Me@users.noreply.github.com>
2021-07-30 17:45:12 +0200

    Add files via upload

---
+++ HaveIBeenPwnedCorrector.js
@@ -0,0 +1,10 @@
+document.addEventListener('change', (event) => {
+    title = document.getElementsByClassName("pwnTitle")
+
+    title[0].getElementsByTagName("h2")[0].innerHTML = "No this time :("
+    document.getElementById("noPwnage").style.background = "linear-gradient(135deg,#320f0f 0,#ba4343 100%)"
+    
+    title[1].getElementsByTagName("h2")[0].innerHTML = "YEAH PWNED!!!"
+    document.getElementsByClassName("pwnedRow")[0].style.background = "linear-gradient(135deg,#0f3212 0,#348f38 100%)"
+    document.getElementsByClassName("pwnedRow")[1].style.background = "linear-gradient(135deg,#0f3212 0,#348f38 100%)"
+})
